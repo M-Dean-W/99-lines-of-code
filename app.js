45260ccf91ddc51ea5daf7c316bdef9f5fef285e
MD <mdeanwill@gmail.com>
2023-11-27 12:21:52 -0800

    uploading lab

---
+++ app.js
@@ -0,0 +1,14 @@
+let friends = ["Jake", "Fatts", "Joe", "Austin", "Ryan"]
+
+for (let i = 0; i < friends.length; ++i) {
+    console.log(friends[i])
+    for (let j = 99; j >= 1; j--) {
+
+        if (j > 1) {
+            console.log([j] + ' lines of code in the file, ' + [j] + ' lines of code; ' + friends[i] + ' strikes one out, clears it all out, ' + [j - 1] + ' lines of code in the file');
+        }
+        else {
+            console.log([j] + ' line of code in the file, ' + [j] + ' line of code; ' + friends[i] + ' strikes one out, clears it all out, ' + ' no more lines of code in the file')
+        }
+    }
+}
